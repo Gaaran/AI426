674a43fee64d5f49e22fd913eeb80292f70efe78
Gaaran <yan.sergent@gmail.com>
2021-05-22 19:31:20 +0200

    Add class interactable

---
+++ Source/AI426/ItemBase/Interactable/Interactable.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemBase/Placeable.h"
+#include "Interactable.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AI426_API AInteractable : public APlaceable
+{
+	GENERATED_BODY()
+	
+};
