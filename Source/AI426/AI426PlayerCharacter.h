64236126985b00462fe3de162f5fd11f64fe8aa3
Gaaran <yan.sergent@gmail.com>
2021-05-22 19:38:40 +0200

    Add PlayerCharacter class

---
+++ Source/AI426/AI426PlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AI426Character.h"
+#include "AI426PlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AI426_API AAI426PlayerCharacter : public AAI426Character
+{
+	GENERATED_BODY()
+	
+};
